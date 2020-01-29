cb2a144d8b7b4df4096a0eea35002175c5a37c97
Jon <jonathanjackson424@gmail.com>
2020-01-29 11:22:14 -0500

    asdf

---
+++ dsa-drills.js
@@ -0,0 +1,16 @@
+//1. It will take 3 steps to find 8.  Each step is below:
+
+// original input: [3, 5, 6, 8, 11, 12, 14, 15, 17, 18]
+
+//1. [3, 5, 6, 8]
+
+//2. [6, 8]
+
+//3. [8]
+
+
+//2. 
+
+
+
+
