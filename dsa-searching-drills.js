fbbeb74514849831ca85032ccedad14c2969bda9
Jon <jonathanjackson424@gmail.com>
2020-01-29 15:58:43 -0500

    added drills file

---
+++ dsa-searching-drills.js
@@ -0,0 +1,17 @@
+//1. It will take 3 steps to find 8.  Each step is below:
+
+// original input: [3, 5, 6, 8, 11, 12, 14, 15, 17, 18]
+
+//1. [3, 5, 6, 8]
+
+//2. [6, 8]
+
+//3. [8]
+
+
+//2. 
+
+// original input: [3, 5, 6, 8, 11, 12, 14, 15, 17, 18]
+
+
+
