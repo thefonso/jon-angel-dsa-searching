2df5a3615f22b969b42f05d379583aa6e1031154
Jon <jonathanjackson424@gmail.com>
2020-01-29 16:35:45 -0500

    star trek

---
+++ StarTrek.js
@@ -0,0 +1,18 @@
+const BinarySearchTree = require('./BinarySearchTree');
+
+let BST = new BinarySearchTree();
+
+BST.insert(5, 'Captain Picard');
+BST.insert(3, 'Commander Riker');
+BST.insert(6, 'Commander Data');
+BST.insert(2, 'Lt. Cmdr. Worf');
+BST.insert(4, 'Lt. Cmdr.LaForge');
+BST.insert(1, 'Lieutenant security-officer');
+BST.insert(6, 'Commander Data');
+BST.insert(8, 'Lt. Cmdr. Crusher');
+BST.insert(7, 'Lieutenant Selar');
+
+
+console.log(BST);
+
+
