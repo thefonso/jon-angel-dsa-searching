974fe9eac9ac021656a9485ea7056c34670b428c
Jon <jonathanjackson424@gmail.com>
2020-01-29 16:47:21 -0500

    prices

--- StarTrek.js
+++ StarTrek.js
@@ -13,6 +13,23 @@ BST.insert(8, 'Lt. Cmdr. Crusher');
 BST.insert(7, 'Lieutenant Selar');
 
 
-console.log(BST);
+//console.log(BST);
 
+
+let pricesArray = [128, 97, 121, 123, 98, 97, 105]
+
+function maxProfit(prices) {
+  if (!prices.length) {
+    return 0;
+  }
+
+  let buy = prices[0], sell = prices[0], profit = 0;
+  for (let i = 1; i < prices.length; ++i) {
+    sell = prices[i];
+    if (sell < buy) buy = sell;
+    if (sell - buy > profit) profit = sell - buy;
+  }
+  return profit;
+}
+console.log(maxProfit(pricesArray));
 
